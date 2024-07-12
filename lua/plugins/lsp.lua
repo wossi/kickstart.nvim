d67966e87849351cd79686e2c9aea65b1fd163fc
Johannes Woske <johannes.woske@deutschebahn.com>
2024-07-12 14:04:02 +0200

    evolving...

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,11 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require('mason-lspconfig').setup {
-        ensure_installed = { 'lua_ls', 'terraformls' },
+        ensure_installed = {
+          'lua_ls',
+          'terraformls',
+          'pyright',
+        },
       }
     end,
   },
@@ -19,6 +23,8 @@ return {
       local lspconfig = require 'lspconfig'
       lspconfig.lua_ls.setup {}
       lspconfig.terraformls.setup {}
+      lspconfig.pyright.setup {}
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
