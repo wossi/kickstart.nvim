19c5b56fa5877df5fb00e46922edb9bf40e2117b
wossi <jwoske@googlemail.com>
2024-08-01 08:00:11 +0200

    highlight yank

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,3 +9,14 @@ vim.cmd("set relativenumber")
 
 vim.cmd("set colorcolumn=120")
 vim.cmd("set cursorline")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
