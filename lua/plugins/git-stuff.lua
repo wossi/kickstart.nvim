69b08c96c6ef8c7526c58536b3799febc7f4da64
Johannes Woske <johannes.woske@deutschebahn.com>
2024-08-06 11:20:53 +0200

    git-plugin (needs lazygit)

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,6 +1,19 @@
 return {
   {
-    "tpope/vim-fugitive",
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
   {
     "lewis6991/gitsigns.nvim",
