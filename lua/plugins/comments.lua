0773e34f3bf13b34f31b246a6f43dcb47afebbaa
Johannes Woske <johannes.woske@deutschebahn.com>
2024-08-07 19:12:14 +0200

    jump through todos

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -10,6 +10,8 @@ return {
         Todo = { icon = " ", color = "info" },
         todo = { icon = " ", color = "info" },
       },
+      vim.keymap.set("n", "<leader>tn", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" }),
+      vim.keymap.set("n", "<leader>tp", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" }),
     },
   },
   {
