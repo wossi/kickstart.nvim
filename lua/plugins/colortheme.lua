1140b1278c5600f24212ee552405b7102ff8b99f
Johannes Woske <johannes.woske@deutschebahn.com>
2025-11-12 10:34:16 +0100

    change colortheme to neon

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,8 +1,8 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("catppuccin-mocha")
+    vim.cmd.colorscheme("cyberdream")
   end,
 }
