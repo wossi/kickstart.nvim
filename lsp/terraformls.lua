5c518e9b09ab62f534622df811a78de8f6b94e6e
Johannes Woske <johannes.woske@deutschebahn.com>
2026-03-31 13:58:07 +0200

    tf logs

--- lsp/terraformls.lua
+++ lsp/terraformls.lua
@@ -1,5 +1,5 @@
 return {
-  cmd = { 'terraform-lsp' },
+  cmd = { 'terraform-lsp', 'serve', '-log-file', vim.fs.dirname(require('vim.lsp.log').get_filename()) .. "/terraform-ls.log" },
   filetypes = { 'terraform', 'hcl', 'opentofu', 'opentofu-vars', 'tf', 'tofu' },
   root_markers = { '.terraform', '.git' },
 }
