ef374bc7398e2955587c490c17cbb2ce9b664150
Johannes Woske <johannes.woske@deutschebahn.com>
2026-05-04 16:17:22 +0200

    mk treesitter run again - reconfigure later

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,34 +2,36 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
     lazy = true,
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
-            },
-            include_surrounding_whitespace = true,
-          },
-        },
-      })
-    end,
+    -- branch = "master",
+    -- config = function()
+    --   require("nvim-treesitter.configs").setup({
+    --     textobjects = {
+    --       select = {
+    --         enable = true,
+    --         lookahead = true,
+    --         keymaps = {
+    --           ["af"] = "@function.outer",
+    --           ["if"] = "@function.inner",
+    --           ["ac"] = "@class.outer",
+    --           ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+    --           ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+    --         },
+    --         selection_modes = {
+    --           ["@parameter.outer"] = "v", -- charwise
+    --           ["@function.outer"] = "V", -- linewise
+    --           ["@class.outer"] = "<c-v>", -- blockwise
+    --         },
+    --         include_surrounding_whitespace = true,
+    --       },
+    --     },
+    --   })
+    -- end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    -- branch = "master",
     opts = {
       ensure_installed = {
         "bash",
@@ -60,10 +62,10 @@ return {
       highlight = { enable = true },
       indent = { enable = true },
     },
-    config = function(_, opts)
-      require("nvim-treesitter.install").prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    -- config = function(_, opts)
+    --   require("nvim-treesitter.install").prefer_git = true
+    --   ---@diagnostic disable-next-line: missing-fields
+    --   require("nvim-treesitter.configs").setup(opts)
+    -- end,
   },
 }
